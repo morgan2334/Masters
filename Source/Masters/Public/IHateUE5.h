237875cf53e3942abd85a2b0789ae0d50965a14e
morgan2334 <44641317+morgan2334@users.noreply.github.com>
2026-07-06 20:37:27 +0100

    iter

---
+++ Source/Masters/Public/IHateUE5.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class MASTERS_API IHateUE5
+{
+public:
+	IHateUE5();
+	~IHateUE5();
+};
